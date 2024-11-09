0fc60b643cc6160689492600844e207158ec4c4e
Malika Temurova <Malika.Temurova@ventionteams.com>
2024-11-09 20:32:03 +0500

    develop: combined backend and frontend for hosting

---
+++ SafeBite.Front/src/app/pages/main-portal/main-portal.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MainPortalRoutingModule } from './main-portal-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MainPortalRoutingModule
+  ]
+})
+export class MainPortalModule { }
